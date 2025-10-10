796eaa28ccb05f1fd0e7be113add77c99209046c
Brittany Buchanan <bbuchanan0400@gmail.com>
2025-10-09 21:38:08 -0400

    Initial commit - created MainApp class + declared helper methods for Home and Ledger screens.

---
+++ src/main/java/com/pluralsight/MainApp.java
@@ -0,0 +1,118 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static Scanner input = null;
+
+    public static void main(String[] args) {
+        input = new Scanner(System.in);
+        boolean running = true;
+
+
+        System.out.println("Welcome!");
+
+
+        while (running) {
+           running = showHomeScreen();
+        }
+    }
+
+    private static boolean showHomeScreen() {
+
+        System.out.println("""
+                
+                                ==================================
+                                          HOME MENU\s
+                                ==================================
+                                Choose an option by letter:
+                               \s
+                                D. Add Deposit\s
+                                P. Make Payment
+                                L. Ledger
+                                X. Exit \
+                """);
+
+        String choice = input.nextLine().trim().toUpperCase();
+
+        switch (choice) {
+
+            case "D":
+                addDeposit();
+
+                break;
+
+            case "P":
+                makePayment();
+                break;
+
+            case "L":
+                showLedgerMenu();
+                break;
+
+            case "X":
+                System.out.println("Thank you for using the accounting ledger. Come again soon");
+                return false;
+            default:
+                System.out.println("Sorry, invalid choice. Please try again.");
+
+
+        }
+        return true;
+    }
+
+    private static void showLedgerMenu() {
+        boolean inLedger = true;
+
+        while (inLedger) {
+
+            System.out.println("""
+                        
+                                        ==================================
+                                                  LEDGER MENU\s
+                                        ==================================
+                                        Choose an option by letter:
+                                       \s
+                                        A. All\s
+                                        D. Deposits
+                                        P. Payments
+                                        R. Reports \s
+                                        H. Home
+                        """);
+
+            String selection = input.nextLine().trim().toUpperCase();
+
+            switch (selection) {
+
+                case "A":
+                    //TODO: Create dAE method
+                    displayAllEntries();
+                    break;
+
+                case "D":
+                    //TODO ; Create dD method
+                    displayDeposits();
+                    break;
+
+                case "P":
+                    //TODO create dP method
+                    displayPayments();
+                    break;
+
+                case "R":
+                    //TODO Create sRS method
+                    showReportsScreen();
+                    break;
+
+                case "H":
+                    inLedger = false;
+                    break;
+                default:
+                    System.out.println("Sorry invalid option. Please try again");
+
+            }
+        }
+
+    }
+}
